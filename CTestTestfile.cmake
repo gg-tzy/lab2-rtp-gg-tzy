67c2e812032d80ce241a1727d9aedfc2c8ca8157
gg-tzy <81548635+gg-tzy@users.noreply.github.com>
2023-03-14 18:38:36 +0800

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tzy/Desktop/lab2-rtp-gg-tzy/Lab2-RTP-Test
+# Build directory: /home/tzy/Desktop/lab2-rtp-gg-tzy/Lab2-RTP-Test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/tzy/Desktop/lab2-rtp-gg-tzy/Lab2-RTP-Test/rtp_test_all[1]_include.cmake")
+subdirs("third_party/googletest-release-1.12.1")
